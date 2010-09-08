60b6895d6791a177c4b82f7970f2759b5db006a1
Jon Maddox <jon@jonsthoughtsoneverything.com>
2010-09-08 11:23:04 -0400

    fix module

--- server.rb
+++ server.rb
@@ -6,7 +6,7 @@ get '/stars.json' do
   page = params[:page] || 0
   offset = page.to_i * 50
   
-  stars = Stars::Star.all(:limit => 50, :offset => offset, :order => 'starred_message_id DESC')
+  stars = StarSearch::Star.all(:limit => 50, :offset => offset, :order => 'starred_message_id DESC')
 
   content_type 'application/json', :charset => 'utf-8'
   {:stars => stars}.to_json
@@ -16,7 +16,7 @@ get '/search.json' do
   page = params[:page] || 0
   offset = page.to_i * 50
 
-  stars = Stars::Star.all({:body => Regexp.new(params[:q], :limit => 50, :offset => offset, :order => 'starred_message_id DESC')})
+  stars = StarSearch::Star.all({:body => Regexp.new(params[:q], :limit => 50, :offset => offset, :order => 'starred_message_id DESC')})
 
   content_type 'application/json', :charset => 'utf-8'
   {:stars => stars}.to_json
